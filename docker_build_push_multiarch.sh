a0004d445019293d83147ae49b0dea03de4ec144
Alexander Gordt <alexander.gordt@objective-partner.com>
2023-11-22 20:55:03 +0100

    two build scripts

---
+++ docker_build_push_multiarch.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+# Prereq.:
+# - Setup docker buildkit -> buildx for platform linux/amd64,linux/arm64
+
+set -e # exit if any failure occurs
+
+echo "############################################################################################################"
+echo " FIRST INSTALL ALL"
+echo "############################################################################################################"
+mvn clean deploy -DskipTests
+
+if [[ "$1" == "" ]];  then
+  echo "Evaluating version..."
+  export VERSION=`mvn help:evaluate -Dexpression=revision -q -DforceStdout`
+else
+  export VERSION="$1"
+fi
+
+echo "VERSION=${VERSION}"
+
+ROOT_DIR=$PWD
+
+echo "############################################################################################################"
+echo " basyx.aasregistry/basyx.aasregistry-service-release-log-mongodb"
+echo "############################################################################################################"
+cd $ROOT_DIR/basyx.aasregistry/basyx.aasregistry-service-release-log-mongodb  || exit
+
+mvn clean deploy \
+    -DskipTests \
+    -Ddocker.provenance=false \
+    -Ddocker.namespace=registry.devinf.objective-partner.net/i40-forks/basyx-java-server-sdk
+
+echo "############################################################################################################"
+echo " basyx.submodelregistry/basyx.submodelregistry-service-release-log-mongodb"
+echo "############################################################################################################"
+cd $ROOT_DIR/basyx.submodelregistry/basyx.submodelregistry-service-release-log-mongodb  || exit
+
+mvn clean deploy \
+    -DskipTests \
+    -Ddocker.provenance=false \
+    -Ddocker.namespace=registry.devinf.objective-partner.net/i40-forks/basyx-java-server-sdk
+
+echo "############################################################################################################"
+echo " basyx.aasrepository/basyx.aasrepository.component"
+echo "############################################################################################################"
+cd $ROOT_DIR/basyx.aasrepository/basyx.aasrepository.component  || exit
+
+docker buildx build \
+  --push \
+  --no-cache \
+  --provenance false \
+  --platform linux/amd64,linux/arm64 \
+  -t registry.devinf.objective-partner.net/i40-forks/basyx-java-server-sdk/aas-repository:$VERSION .
+
+echo "############################################################################################################"
+echo " basyx.conceptdescriptionrepository/basyx.conceptdescriptionrepository.component"
+echo "############################################################################################################"
+cd $ROOT_DIR/basyx.conceptdescriptionrepository/basyx.conceptdescriptionrepository.component  || exit
+
+docker buildx build \
+  --push \
+  --no-cache \
+  --provenance false \
+  --platform linux/amd64,linux/arm64 \
+  -t registry.devinf.objective-partner.net/i40-forks/basyx-java-server-sdk/cd-repository:$VERSION .
+
+echo "############################################################################################################"
+echo " basyx.submodelrepository/basyx.submodelrepository.component"
+echo "############################################################################################################"
+cd $ROOT_DIR/basyx.submodelrepository/basyx.submodelrepository.component  || exit
+
+docker buildx build \
+  --push \
+  --no-cache \
+  --provenance false \
+  --platform linux/amd64,linux/arm64 \
+  -t registry.devinf.objective-partner.net/i40-forks/basyx-java-server-sdk/submodel-repository:$VERSION .
